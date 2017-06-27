4f22eaec0386eb4ae4724a8c199e05948e98b14b
kitesoo <kite_90@naver.com>
2017-06-27 17:20:59 +0900

    오타수정, sql오타수정

---
+++ turnUp-fridger/src/kr/co/turnup_fridger/vo/RecipeCrse.java
@@ -0,0 +1,14 @@
+package kr.co.turnup_fridger.vo;
+
+import java.io.Serializable;
+
+public class RecipeCrse implements Serializable{
+
+	private int recipeId;
+	private int cookingNo;
+	private String cookingDc;
+	private String stepImageUrl;
+	private String stepTip;
+	private RecipeInfo recipeInfo;
+	
+}
--- turnUp-fridger/src/kr/co/turnup_fridger/vo/ViewTnRecipeInfo.java
+++
@@ -1,7 +0,0 @@
-package kr.co.turnup_fridger.vo;
-
-import java.io.Serializable;
-
-public class ViewTnRecipeInfo implements Serializable{
-
-}
