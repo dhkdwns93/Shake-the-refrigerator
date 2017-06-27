6813ecf0971bf128f068a506c46de75d3b55eec7
kitesoo <kite_90@naver.com>
2017-06-27 15:02:42 +0900

    나의식재료, 레시피관련, 재료보관 vo생성

---
+++ turnUp-fridger/src/kr/co/turnup_fridger/vo/IrdntManage.java
@@ -0,0 +1,17 @@
+package kr.co.turnup_fridger.vo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class IrdntManage implements Serializable{
+
+	private int myIrdntsKey;
+	private Date startDate;
+	private Date endDate;
+	private int freshLevel;
+	private String irdntCount;
+	private int irdntId;
+	private int fridgerId;
+	
+	
+}
